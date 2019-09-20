63b8dcd44ab788707e8ab305fef9d0111da4051b
Poochofpain <poochofpain92@gmail.com>
2019-09-19 23:41:46 -0400

    Hex to Decimal Working

--- Hex.py
+++ Hex.py
@@ -3,7 +3,7 @@
 
 import sys
 
-def get_nums() -> List[str]:
+def get_nums():
     ''' This function returns a list of numbers to convert. '''
     lst = []
     if len(sys.argv) < 2:
@@ -18,14 +18,43 @@ def get_nums() -> List[str]:
         # We need to verify that the hex value is valid,
         # but it's better to do that while we calculate
         # the decimal value of it.
-            if arg[:2] == '0x' or arg[:2].isdigit():
+            if arg[:2] == '0x' or arg.isdigit():
                 lst.append(arg)
 
         return lst     
 
-def calculate(nums: List[str]) -> List[str]:
+def calculate(nums):
+    ''' If the number is a decimal number convert it to hex,
+        othersize check if the number is a valid hex value
+        then convert it to decimal. '''
 
-
-
-def print_ans(nums: List[str], ans: List[str]):
+    valid = ['1', '2', '3', '4', '5', '6', '7', '8', '9',
+             'a', 'A', 'b', 'B', 'c', 'C', 'd', 'D', 'e',
+             'E', 'f', 'F']
+    output = []
+    flag = True
     
+    for num in nums:
+        if num[:2:] == '0x':
+            # Check the digits. If they're valid turn them into decimal
+            for digit in num[2::]:
+                if digit in valid:
+                    flag = True
+                else:
+                    flag = False
+
+            if flag:
+                output.append(int(num, 16))
+            else:
+                output.append("Invalid number!")
+        #else:
+            # Do the conversion to hex
+            
+
+    return output
+    
+
+# Do the stuff
+numbers = get_nums()
+answers = calculate(numbers)
+print(answers)
